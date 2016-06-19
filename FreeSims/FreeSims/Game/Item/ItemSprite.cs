19e825a9c13f9cdbc485895d70dcf7b45260793f
ItzBlueMan <itzblueman@gmail.com>
2016-06-19 13:00:24 +0200

    Added table sprite

--- FreeSims/FreeSims/Game/Item/ItemSprite.cs
+++ FreeSims/FreeSims/Game/Item/ItemSprite.cs
@@ -7,11 +7,13 @@ namespace Julien12150.FreeSims.Game.Item
     {
         public Texture2D oldTv;
         public Texture2D Chair;
+		public Texture2D Table;
 		
         public ItemSprite(ContentManager Content)
         {
             oldTv = Content.Load<Texture2D>("Item/OldTV");
 			Chair = Content.Load<Texture2D>("Item/Chair");
+			Table = Content.Load<Texture2D>("Item/Table");
         }
     }
 }
