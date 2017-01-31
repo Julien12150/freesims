ea8794bda39ccbf72b810366b0a2f87f42818c59
Technochips <julienfillol2@gmail.com>
2017-01-31 01:04:20 +0100

    fixed wrong executable structure on linux
    moved /usr/bin/Freesims to /usr/share/freesims
    you can run freesims on a terminal

--- setup_deb.sh
+++ setup_deb.sh
@@ -1,12 +1,12 @@
-rm -r freesims_0.0-0/usr/bin
-mkdir freesims_0.0-0/usr/bin
-mkdir freesims_0.0-0/usr/bin/FreeSims
-cp -r FreeSims/FreeSims/bin/x86/Release/Content freesims_0.0-0/usr/bin/FreeSims/
-cp -r FreeSims/FreeSims/bin/x86/Release/Language freesims_0.0-0/usr/bin/FreeSims/
-cp FreeSims/FreeSims/bin/x86/Release/FreeSims.exe freesims_0.0-0/usr/bin/FreeSims/
-cp FreeSims/FreeSims/bin/x86/Release/MonoGame.Framework.dll freesims_0.0-0/usr/bin/FreeSims/
-cp FreeSims/FreeSims/bin/x86/Release/OpenTK.dll freesims_0.0-0/usr/bin/FreeSims/
-cp FreeSims/FreeSims/bin/x86/Release/NVorbis.dll freesims_0.0-0/usr/bin/FreeSims/
-cp FreeSims/FreeSims/Icon.ico freesims_0.0-0/usr/bin/FreeSims/
+rm -r freesims_0.0-0/usr/share/freesims
+mkdir freesims_0.0-0/usr/share/freesims
+cp -r FreeSims/FreeSims/bin/x86/Release/Content freesims_0.0-0/usr/share/freesims/
+cp -r FreeSims/FreeSims/bin/x86/Release/Language freesims_0.0-0/usr/share/freesims/
+cp FreeSims/FreeSims/bin/x86/Release/FreeSims.exe freesims_0.0-0/usr/share/freesims/
+cp FreeSims/FreeSims/bin/x86/Release/MonoGame.Framework.dll freesims_0.0-0/usr/share/freesims/
+cp FreeSims/FreeSims/bin/x86/Release/OpenTK.dll freesims_0.0-0/usr/share/freesims/
+cp FreeSims/FreeSims/bin/x86/Release/NVorbis.dll freesims_0.0-0/usr/share/freesims/
+cp FreeSims/FreeSims/Icon.ico freesims_0.0-0/usr/share/freesims/
 dpkg-deb --build freesims_0.0-0
+mkdir Output
 mv freesims_0.0-0.deb Output/freesims_setup.deb
