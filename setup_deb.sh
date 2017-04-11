65c938e2e73d8868531af533d88736f16bd0dc79
Technochips <julienfillol2@gmail.com>
2017-04-12 01:52:24 +0200

    made script add the svg icon in the pixmaps folder also .desktop file uses that svg file

--- setup_deb.sh
+++ setup_deb.sh
@@ -1,5 +1,6 @@
 rm -r freesims_0.0-0/usr/share/freesims
-mkdir freesims_0.0-0/usr/share/freesims
+mkdir -p freesims_0.0-0/usr/share/freesims
+mkdir -p freesims_0.0-0/usr/share/pixmaps
 cp -r FreeSims/FreeSims/bin/x86/Release/Content freesims_0.0-0/usr/share/freesims/
 cp -r FreeSims/FreeSims/bin/x86/Release/Language freesims_0.0-0/usr/share/freesims/
 cp FreeSims/FreeSims/bin/x86/Release/FreeSims.exe freesims_0.0-0/usr/share/freesims/
@@ -7,6 +8,7 @@ cp FreeSims/FreeSims/bin/x86/Release/MonoGame.Framework.dll freesims_0.0-0/usr/s
 cp FreeSims/FreeSims/bin/x86/Release/OpenTK.dll freesims_0.0-0/usr/share/freesims/
 cp FreeSims/FreeSims/bin/x86/Release/NVorbis.dll freesims_0.0-0/usr/share/freesims/
 cp FreeSims/FreeSims/Icon.ico freesims_0.0-0/usr/share/freesims/
+cp FreeSims/FreeSims/Icon.svg freesims_0.0-0/usr/share/pixmaps/freesims.svg
 dpkg-deb --build freesims_0.0-0
 mkdir Output
 mv freesims_0.0-0.deb Output/freesims_setup.deb
